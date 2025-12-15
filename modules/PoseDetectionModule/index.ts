bc7935f4f2feca2fea4eedb42a9ad78613a8cba2
takaxis2 <axis1130@gmail.com>
2025-12-16 02:14:59 +0900

    reset main branch

--- modules/PoseDetectionModule/index.ts
+++
@@ -1,6 +0,0 @@
-// Reexport the native module. On web, it will be resolved to PoseDetectionModule.web.ts
-// and on native platforms to PoseDetectionModule.ts
-export { default } from './src/PoseDetectionModule';
-export * from './src/PoseDetectionModule.types';
-export { default as PoseDetectionModuleView } from './src/PoseDetectionModuleView';
-
